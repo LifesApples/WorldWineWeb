dcc845d43b74fbe5d6b6b8a831cb73e790e427ff
NimaFaghihi <nimafaghihi99@gmail.com>
2022-04-05 19:43:51 +0200

    Alla filer och grundläggande kod

---
+++ website/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+
+def create_app():
+    '''
+    Ramverket flask kopplar till alla delar i programmet.
+    '''
+    app = Flask(__name__)
+    app.config ['SECRET_KEY'] = 'WWW'
+
+    #Hämtar veriablerna från de skapade filerna.
+    from .views import views 
+    from .auth import auth 
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
+
+    return app
