1ff876a6bf609c4e3acef299bbdb806d50c6a976
azamsuleiman <azam.suleiman@gmail.com>
2022-04-19 17:50:11 +0200

    Implemented register functionality

--- WWW/website/auth.py
+++ WWW/website/auth.py
@@ -1,13 +1,28 @@
 from flask import Blueprint, render_template, request, flash
 
-
 auth = Blueprint('auth', __name__)
 
+import psycopg2
+
+try:
+    conn = psycopg2.connect(host = 'pgserver.mau.se',
+                database = 'am4404',
+                user = 'am4404',
+                password = 'zxd0hy59')
+    
+except Exception as error:
+    print(error)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    print("Tried logging in")
+    if request.method == 'POST':
+        username = request.form.get("form2Example11")
+        print("Name was: " + str(username))
+        username = request.form.get("Password")
+        
 
+    return render_template("login.html")
 
 @auth.route("/logout")
 def loginout():
@@ -16,7 +31,7 @@ def loginout():
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
-
+    print("Tried registering")
     if request.method == 'POST':
         username = request.form.get("username")
         email = request.form.get("email")
@@ -35,4 +50,20 @@ def sign_up():
         else: 
             flash("Account created!", category="success")
 
+        try:
+                cur = conn.cursor()
+
+                insert_script = 'SET search_path = "WorldWineWeb", am4404, public; INSERT INTO users(username, password, email) VALUES (%s, %s, %s)'
+                data = (username,email,password1)
+                cur.execute(insert_script,data)
+                conn.commit()
+               
+        except Exception as error:
+            print(error)
+            
+        finally:
+            if cur is not None:
+                cur.close()
+            if cur is not None:
+                conn.close()
     return render_template("sign_up.html")
