b5d8b898cb96cb1771fb9ca71340b415bf5c281b
azamsuleiman <azam.suleiman@gmail.com>
2022-05-12 23:18:32 +0200

    Rensade i scraper klassen och kommenterat koden

--- WWW/website/scraper.py
+++ WWW/website/scraper.py
@@ -1,8 +1,11 @@
 from math import prod
 import os
+import re
+from this import d
 import requests
 import json
 import urllib.request, urllib.error, urllib.parse
+
 from db_connection import *
 
 from urllib.parse import urlencode, urlunparse,urlparse
@@ -12,93 +15,86 @@ from flask import Blueprint, render_template
 
 scraper = Blueprint('scraper', __name__)
 
+#ImgScraper for adding picture links to database
 class scraper:
 
     def __init__(self):
         pass
-    #query = "Absolut vodka"
-    #url = urlunparse(("https", "www.bing.com", "images/search?q=", "", urlencode({"q": query}), ""))
+    #Function for searching for images using database values and updating database with links
+    def searchpics(productname,bottletextshort,volume,categorylevel1):
+        
+        nonformatedtext = '%s %s %s %s' % (productname,volume,bottletextshort,categorylevel1)
+        nonformatedtext = str(nonformatedtext).encode('utf-8') #Endcode to avoid the request from throwing errors for åäö 
+        nonformatedtext = str(nonformatedtext).replace('b', '') #Remove the letter B that keeps showing upp in the list
+        nonformatedtext = str(nonformatedtext).replace("'", '') #Remove quote from each element 
 
-    def searchPics(pname, producer, bottletype):
-        seartext = pname+" "+producer+" "+bottletype
-        print("Text: " + seartext)
-        count = 4
-        sear=seartext.strip()
-        sear=sear.replace(' ','+')
-        URL='https://bing.com/images/search?q=' + sear
+        #Build search query
+        searchtext=nonformatedtext.strip()
+        searchtext = str(searchtext)
+        searchtext=searchtext.replace(' ','+')
+        
+        #Search and get results
+        URL='https://bing.com/images/search?q=' + searchtext
             #+ '&safeSearch=' + adlt + '&count=' + count
         custom_user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100101 Firefox/65.0"
         req = Request(URL, headers={"User-Agent": custom_user_agent})
         page = urlopen(req)
-        # Further code I've left unmodified
+        
+        #Get results, loops through results and return first value that meets the criteria. Return as URL (for the image)
         soup = BeautifulSoup(page.read(),features="html.parser")
-        #links = soup.findAll("jpg")
-
-        ActualImages=[]# contains the link for Large original images, type of  image
         wow = soup.find_all('a',class_='iusc')
         for i in wow:
             try:
-                print(eval(i['m'])['murl'])
-                print()
-                break
+                 url = eval(i['m'])['murl']
+                 print(url)
+                 return url
             except:
-                pass
+               pass
+
+        
+    #This loop gets the first product that does not have a URL and uses searchproduct() to find a matching image
+    #This operation is done 500 times or until all URL fields have a value (a link)  
+    count = 500 
+    for i in range(count):
+        #Get values from product table where url is empty
+        cur = conn.cursor()
+        insert_script = 'SET search_path = "WorldWineWeb", am4404, public; select productname,bottletextshort,volume,categorylevel1 from product where "url" is NULL;'
+        cur.execute(insert_script)
+        #Get returned records and convert results to a list
+        records = cur.fetchone()
+        text = list(records)
+        text = str(text).split(",")
     
-
-    searchPics("Sju komma tvaan" , "Ljus lager", " Starkare lager")
+        #Loop through results to create a list within a list and formate text along the way
+        for counter,i in enumerate(text):
+            text[counter] = str(text[counter]).replace('[', '')# remove open bracket from each element
+            text[counter] = str(text[counter]).replace(')', '')# remove parenthesis from each element
+            text[counter] = str(text[counter]).replace("'", '')# remove singlequote from each element
+            text[counter] = str(text[counter]).replace("]", '')# remove close bracket from each element
+            #Store first value without removing å,ä,ö. Needed in order to query database later
+            if(counter == 0):
+                orgValue = text[0]
+            text[counter] = str(text[counter]).replace("å", 'a')# remove å from each element
+            text[counter] = str(text[counter]).replace("ä", 'a')# remove ä from each element
+            text[counter] = str(text[counter]).replace("ö", 'o')# remove ö from each element
+            text[counter] = str(text[counter]).replace("Å", 'a')# remove Å from each element
+            text[counter] = str(text[counter]).replace("Ä", 'a')# remove Ä from each element
+            text[counter] = str(text[counter]).replace("Ö", 'o')# remove Ö from each element
+            #If a field contains None remove it
+            if(text[counter] == None):
+                    text[counter] = ""
     
-    """""
-    for a in soup.find_all("a",{"class":"iusc"}):
-        print(a)
-        # mad = json.loads(a["mad"])
-        # turl = mad["turl"]
-        m = json.loads(a["m"])
-        murl = m["murl"]
-        turl = m["turl"]
-        print("URL Should be: " + murl)
-
-        image_name = urllib.parse.urlsplit(murl).path.split("/")[-1]
-        print("Name is: " + image_name)
-
-        ActualImages.append((image_name, turl, murl))
-
-    print("there are total" , len(ActualImages),"images")
-    """
-    """"
-    ------------------------------------------------------
-    for link in links:
-        print("Denna " + link["href"])
-
-    """
-    """
-    from google_images_search import GoogleImagesSearch
-
-    ##response = requests.get( , data = {'key':'AIzaSyBRZHKkHm_WpiMzQM4z6W-YEVKSedRv3Ck'})
-
-    headers = {
-        # Request headers
-
-    }
-    apiKey = "AIzaSyBRZHKkHm_WpiMzQM4z6W-YEVKSedRv3Ck"
-    project = "34b8f4687e269165a"
-    gis = GoogleImagesSearch(apiKey,project)
-    test = requests.get("https://www.googleapis.com/customsearch/v1?key=AIzaSyBRZHKkHm_WpiMzQM4z6W-YEVKSedRv3Ck"
-                        "&cx=017576662512468239146:omuauf_lfve&q=lectures,developerKey=key")
-
-
-    _search_params = {
-        'q'       : 'Persian Cat',
-        'num'     : 10,
-        'safe'    : 'high',
-        'fileType': 'jpg|png',
-        'imgType' : 'photo',
-    }
-    # search first, then download and resize afterwards:
-    gis.search(search_params=_search_params,  custom_image_name='persian')
-
-    for image in gis.results():
-        image.download('/Users/azamsuleiman/Downloads/Katter') #download images to the directory
-        image.resize(500, 500) # resize image
-
-    os.listdir('/Users/azamsuleiman/Downloads/Katter')
-    """
+        #Search for image using the above list, a url is returned. Use the unchanged value (that contains å,ä,ö) and
+        #use it in the query for the database.
+        urlToSend = searchpics(text[0],text[1],text[2], text[3])   
+        query = "SET search_path = 'WorldWineWeb', am4404, public; select updateurl('%s','%s');" % (orgValue,urlToSend)
+        
+        #Used for checking if update was made to the database   
+        try:
+            cur.execute(query)
+            conn.commit()
+            result = cur.rowcount
+            for rows in cur:
+                print("result after commit: ", rows)
+        except Exception as error:
+            print("Error: ", error)
