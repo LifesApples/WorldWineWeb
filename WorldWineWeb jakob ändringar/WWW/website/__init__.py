49d39553c709433a1405ffa631547898e82021df
jakobw98 <jakob.wind@hotmail.com>
2022-05-16 13:44:04 +0200

    Påbörjat kommentarsfält

---
+++ WorldWineWeb jakob ändringar/WWW/website/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'WWW'
+
+    # Importerar allt från template, static o.s.v.
+    from .views import views
+    from .auth import auth
+    from .comments import comments
+    ##from .scraper import scraper
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+    app.register_blueprint(comments, url_prefix='/')
+    ## app.register_blueprint(scraper, url_prefix='/')
+    ## imageScraper = scraper()
+    ## imageScraper.searchPics("Three Hearts","Krönleins","Burk")
+   
+
+    return app
+
+
+
