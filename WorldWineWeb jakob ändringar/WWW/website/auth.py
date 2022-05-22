22dd2b36ddd59b78267346fba7d175d4db2363cf
jakobw98 <jakob.wind@hotmail.com>
2022-05-22 16:34:48 +0200

    påbörjan av kommentarsfält ändringar
    Co-Authored-By: klau22 <102652305+klau22@users.noreply.github.com>
    Co-Authored-By: ChrisMols <101338263+ChrisMols@users.noreply.github.com>

---
+++ WorldWineWeb jakob ändringar/WWW/website/auth.py
@@ -0,0 +1,93 @@
+from unicodedata import name
+from flask import Blueprint, render_template, request, flash
+from db_connection import *
+
+# Länkar auth.py med main.py.
+auth = Blueprint('auth', __name__)
+
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    """
+    Hämtar alla fält och försöker logga in användaren
+    """
+    if request.method == 'POST':
+        username = request.form.get("username")
+        print("Name was: " + username)
+        password = request.form.get("password1")
+        print("Password was: " + password)
+        
+        
+        try:
+            
+            insert_script = 'SET search_path = "WorldWineWeb", am4404, public; Select * from users where username = %s and "password"= %s'
+            data = (username,password)
+            print("Testing: " + insert_script)
+            cur.execute(insert_script,data)
+            print("The number of parts: ", cur.rowcount)
+            conn.commit()
+
+            if cur.rowcount < 1:
+                flash("Wrong username or password", category="error")
+            else:
+                flash("Login sucessful!", category="success")
+            
+        except Exception as error:
+            print(error)
+                
+     
+    return render_template("login.html")
+
+@auth.route("/logout")
+def loginout():
+    """
+    Loggar ut användaren
+    """
+    cur = conn.cursor()
+    insert_script = 'SET search_path = "WorldWineWeb", am4404, public; Select getaproducts(10)'
+    cur.execute(insert_script)
+    print("Returned products: " + str(cur.rowcount))
+    return "<p>Loginout</p>"
+
+
+@auth.route('/sign-up', methods=['GET', 'POST'])
+def sign_up():
+    """
+    Hämtar all information från fältet och registerar användaren i db.
+    """
+    print("Tried registering")
+    if request.method == 'POST':
+        username = request.form.get("username")
+        email = request.form.get("email")
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")
+
+        #krav på användaren input med hjälp av en if-sats.
+        if len(username) < 4:
+            flash("Username must be greater than 4 characters", category="error")
+        elif len(email) < 8:
+            flash("Email must be greater than 8 characters", category="error")
+        elif password1 != password2:
+            flash("Passwords dont match", category="error")
+        elif len(password1) < 7:
+            flash("Password must be at least 7 characters.", category="error")
+        else: 
+            flash("Account created!", category="success")
+
+        try:
+                cur = conn.cursor()
+
+                insert_script = 'SET search_path = "WorldWineWeb", am4404, public; INSERT INTO users(username, password, email) VALUES (%s, %s, %s)'
+                data = (username,email,password1)
+                cur.execute(insert_script,data)
+                conn.commit()
+               
+        except Exception as error:
+            print(error)
+            
+        finally:
+            if cur is not None:
+                cur.close()
+            if cur is not None:
+                conn.close()
+    return render_template("sign_up.html")
