49bf1b765ea7a347589ebe6a58cf21cbeeccd29a
ChrisMols <101338263+ChrisMols@users.noreply.github.com>
2022-05-26 13:48:16 +0200

    Omoss route
    Co-Authored-By: jakobw98 <102788220+jakobw98@users.noreply.github.com>
    Co-Authored-By: klau22 <102652305+klau22@users.noreply.github.com>

---
+++ WorldWineWeb-main/WWW/website/scraper.py
@@ -0,0 +1,101 @@
+from math import prod
+import os
+import re
+from this import d
+import requests
+import json
+import urllib.request, urllib.error, urllib.parse
+
+from db_connection import *
+
+from urllib.parse import urlencode, urlunparse,urlparse
+from urllib.request import urlopen, Request
+from bs4 import BeautifulSoup
+from flask import Blueprint, render_template
+
+scraper = Blueprint('scraper', __name__)
+
+#ImgScraper klass för att lägga till bilder i databasen
+class scraper:
+
+    def __init__(self):
+        pass
+    #Funktion som söker efter bilder och uppdaterar databasen med URL till sökreusltatet/bilden.
+    def searchpics(productname,bottletextshort,volume,categorylevel1):
+        
+        
+        #Formaterar och tar bort extra tecken från texten som kommer från funktionens parametrar
+        nonformatedtext = '%s %s %s %s' % (productname,volume,bottletextshort,categorylevel1)
+        nonformatedtext = str(nonformatedtext).encode('utf-8') #Formaterar kod så att å,ä,ö blir läsbart. 
+        nonformatedtext = str(nonformatedtext).replace('b', '') #Tar bort värdet b som kommer med i hämtningen
+        nonformatedtext = str(nonformatedtext).replace("'", '') #Tar bort citattecken
+        searchtext=nonformatedtext.strip() 
+        searchtext = str(searchtext)
+        searchtext=searchtext.replace(' ','+')
+        
+        #Bygger en söksträng som sedan används i sökningen
+        URL='https://bing.com/images/search?q=' + searchtext
+            #+ '&safeSearch=' + adlt + '&count=' + count
+        custom_user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100101 Firefox/65.0"
+        req = Request(URL, headers={"User-Agent": custom_user_agent})
+        page = urlopen(req)
+        
+        #Hämta sökresultat och hämta första som är en bild länk
+        soup = BeautifulSoup(page.read(),features="html.parser")
+        wow = soup.find_all('a',class_='iusc')
+        for i in wow:
+            try:
+                 url = eval(i['m'])['murl']
+                 print("Link: ", url)
+                 return url
+            except:
+               pass
+
+    
+    #Loopar 500 gånger eller tills alla url fält har ett värde (en url). 
+    count = 0 
+    for i in range(count):
+        #Hämtar en rad från product tabellen där url är tomt
+        cur = conn.cursor()
+        insert_script = 'SET search_path = "WorldWineWeb", am4404, public; select productname,bottletextshort,volume,categorylevel1 from product where "url" is NULL;'
+        cur.execute(insert_script)
+        #Hämtar raden, delar upp den och lägger i en lista
+        records = cur.fetchone()
+        text = list(records)
+        text = str(text).split(",")
+    
+        #Loopar igenom listan och formaterar texten.
+        for counter,i in enumerate(text):
+            #Formaterar text för varje rad
+            text[counter] = str(text[counter]).replace('[', '')
+            text[counter] = str(text[counter]).replace(')', '')
+            text[counter] = str(text[counter]).replace("'", '')
+            text[counter] = str(text[counter]).replace("]", '')
+            #Spara första värdet (produktnamnet) så att inte å,ä,ö tas bort. Behövs senare för att bygga strängen till databasen
+            if(counter == 0):
+                orgValue = text[0]
+            #Ta bort å,ä,ö för varje element i listan.
+            text[counter] = str(text[counter]).replace("å", 'a')# remove å from each element
+            text[counter] = str(text[counter]).replace("ä", 'a')# remove ä from each element
+            text[counter] = str(text[counter]).replace("ö", 'o')# remove ö from each element
+            text[counter] = str(text[counter]).replace("Å", 'a')# remove Å from each element
+            text[counter] = str(text[counter]).replace("Ä", 'a')# remove Ä from each element
+            text[counter] = str(text[counter]).replace("Ö", 'o')# remove Ö from each element
+            #Töm fältet om den innehåller None
+            if(text[counter] == None):
+                    text[counter] = ""
+    
+        #Söker efter en bild med värden från ovan lista. En url till bilden returneras.
+        #Uppdatera sedan databasen med ny länk genom att kalla på funktionen updateurl()
+        urlToSend = searchpics(text[0],text[1],text[2], text[3])   
+        query = "SET search_path = 'WorldWineWeb', am4404, public; select updateurl('%s','%s');" % (orgValue,urlToSend)
+        
+        #Används för att kolla på vad databasen svarar
+        try:
+            cur.execute(query)
+            conn.commit()
+            result = cur.rowcount
+            for rows in cur:
+                print("result after commit: ", rows)
+        except Exception as error:
+            print("Error: ", error)
---
+++ WorldWineWeb-main/WWW/website/views.py
@@ -0,0 +1,116 @@
+import this
+from unittest import result
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from db_connection import *
+
+# Definierar views som blueprints för att vi ska kunna använda det som templates.
+views = Blueprint('views', __name__)
+
+# Routar ålderkontroll.
+@views.route('/')
+def start():
+    return render_template("age_control.html")
+
+@views.route('/omoss.html')
+def omoss():
+    return render_template("omoss.html")
+
+# Routar hem.
+@views.route('/home', methods=['GET', 'POST'])
+def home():
+
+    if request.method == 'POST':
+        search_product = request.form.get("searchfunction")
+        print(search_product)
+        print("Products found!")
+        cur = conn.cursor()
+        insert_script = "SET search_path = 'WorldWineWeb', am4404, public; select searchProduct('%s')" % (search_product)
+        print(insert_script)
+        cur.execute(insert_script)
+        records = cur.fetchall()
+        data = []
+        data = list(records)
+        rows = []
+        for i in enumerate(data):
+            ##Lägger till värden för en produkt i en lista
+            splitColumns = str(i).split(",")
+            splitColumns = [j.replace('"', '') for j in splitColumns]
+            splitColumns = [j.replace('(', '') for j in splitColumns]
+            splitColumns = [j.replace(')', '') for j in splitColumns]
+            splitColumns = [j.replace("'", '') for j in splitColumns] 
+            ###Lägger till listan i en lista
+            rows.append(splitColumns)
+        
+        conn.commit()
+   
+    
+        if search_product != '':
+            print(rows)
+            return render_template("products.html", rows=rows)
+            
+
+
+    return render_template("index.html")
+
+# Routar till info om varför man inte kommer åt hemsidan
+@views.route('/decline')
+def decline():
+    return render_template("decline.html")
+
+# Routar produktsida 
+@views.route("/products/<productid>")
+def show_product(productid):
+    cur = conn.cursor()
+    insert_script = "SET search_path = 'WorldWineWeb', am4404, public; select * from product where productid ='%s'" % (productid)
+    cur.execute(insert_script)
+    print(insert_script + "här")
+    records = cur.fetchall()
+    conn.commit()
+    data = []
+    data = list(records)
+    rows = []
+    #Skapar en lista i en lista där den innre listan innehåller värden för 1 produkt.
+    for i in enumerate(data):
+        #Formatterar texten från databasen
+        splitColumns = str(i).split(",")
+        splitColumns = [j.replace('"', '') for j in splitColumns]
+        splitColumns = [j.replace('(', '') for j in splitColumns] 
+        splitColumns = [j.replace(')', '') for j in splitColumns] 
+        splitColumns = [j.replace("'", '') for j in splitColumns]
+        #Lägger till listan i en lista för att skapa en produktkatalog
+        rows.append(splitColumns)
+      
+    
+                     
+    return render_template("product_page.html", productid = productid, rows=rows)
+
+
+# Routar produktkatalogen 
+@views.route('/products')
+def product():
+    print("Products found!")
+    cur = conn.cursor()
+    insert_script = 'SET search_path = "WorldWineWeb", am4404, public; Select getproducts(20)'
+    cur.execute(insert_script)
+    records = cur.fetchall()
+    data = []
+    data = list(records)
+    rows = []
+    for i in enumerate(data):
+        ##Lägger till värden för en produkt i en lista
+        splitColumns = str(i).split(",")
+        splitColumns = [j.replace('"', '') for j in splitColumns]
+        splitColumns = [j.replace('(', '') for j in splitColumns]
+        splitColumns = [j.replace(')', '') for j in splitColumns]
+        splitColumns = [j.replace("'", '') for j in splitColumns] 
+        ###Lägger till listan i en lista
+        rows.append(splitColumns)
+      
+    conn.commit()
+    return render_template("products.html", rows=rows)
+
+    
+
+
+
+
