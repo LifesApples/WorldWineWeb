25567c7a73ea6f4cc49b6468e114ab909a8091ca
klau22 <klaudia.uni2021@gmail.com>
2022-05-31 14:27:03 +0200

    CSS+HTML + JS TEST

---
+++ WWW/website/js/loader.js
@@ -0,0 +1,11 @@
+const loader = document.querySelector('.loader');
+const main = document.querySelector('.main');
+
+/* function init(){
+    setTimeout(() =>{
+        loader.style.opacity = 0;
+        loader.style.display = 'none';
+        
+    })
+}
+*/
