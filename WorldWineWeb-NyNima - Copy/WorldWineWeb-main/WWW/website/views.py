1771c1f3698f051809191859416682f812d90cc5
ChrisMols <101338263+ChrisMols@users.noreply.github.com>
2022-06-04 13:38:18 +0200

    Ny chris commit
    Ändrat HTML/CSS och kategori
    
    Co-Authored-By: jakobw98 <102788220+jakobw98@users.noreply.github.com>

--- WorldWineWeb-NyNima - Copy/WorldWineWeb-main/WWW/website/views.py
+++ WorldWineWeb-NyNima - Copy/WorldWineWeb-main/WWW/website/views.py
@@ -11,18 +11,52 @@ views = Blueprint('views', __name__)
 def start():
     return render_template("age_control.html")
 
-@views.route('/omoss.html')
+@views.route('/omoss', methods=['GET', 'POST'])
 def omoss():
-    return render_template("omoss.html")
+
+        if request.method == 'POST':
+            search_product = request.form.get("searchfunction")
+            print(search_product)
+            print("Products found!")
+            cur = conn.cursor()
+            insert_script = "SET search_path = 'WorldWineWeb', am4404, public; select searchProduct('%s')" % (search_product)
+            print(insert_script)
+            cur.execute(insert_script)
+            records = cur.fetchall()
+            data = []
+            data = list(records)
+            rows = []
+            for i in enumerate(data):
+                ##Lägger till värden för en produkt i en lista
+                splitColumns = str(i).split(",")
+                splitColumns = [j.replace('"', '') for j in splitColumns]
+                splitColumns = [j.replace('(', '') for j in splitColumns]
+                splitColumns = [j.replace(')', '') for j in splitColumns]
+                splitColumns = [j.replace("'", '') for j in splitColumns] 
+                ###Lägger till listan i en lista
+                rows.append(splitColumns)
+            
+            conn.commit()
+    
+        
+            if search_product != '':
+                print(rows)
+                return render_template("products.html", rows=rows)
+
+        return render_template("omoss.html")
+
+@views.route("/comments")
+def comments():
+    return render_template("comments.html")
 
 # Routar hem.
 @views.route('/home', methods=['GET', 'POST'])
 def home():
+    print("Home route was called")
 
     if request.method == 'POST':
         search_product = request.form.get("searchfunction")
         print(search_product)
-        print("Products found!")
         cur = conn.cursor()
         insert_script = "SET search_path = 'WorldWineWeb', am4404, public; select searchProduct('%s')" % (search_product)
         print(insert_script)
@@ -60,6 +94,7 @@ def decline():
 # Routar produktsida 
 @views.route("/products/<productid>")
 def show_product(productid):
+    print("produktid i route produktsida" + productid)
     cur = conn.cursor()
     insert_script = "SET search_path = 'WorldWineWeb', am4404, public; select * from product where productid ='%s'" % (productid)
     cur.execute(insert_script)
@@ -81,16 +116,49 @@ def show_product(productid):
         rows.append(splitColumns)
       
     
-                     
+    cur = conn.cursor()
+    insert_script1 = "SET search_path = 'WorldWineWeb', am4404, public; select getproductcomments('%s')" % (productid)
+    cur.execute(insert_script1)
+    print(insert_script1 + "här")
+    records = cur.fetchall()
+    for comments in records:
+        print(comments)
+    conn.commit()
+                
     return render_template("product_page.html", productid = productid, rows=rows)
 
 
 # Routar produktkatalogen 
-@views.route('/products')
+@views.route('/products', methods=['GET', 'POST'])
 def product():
+    
     print("Products found!")
     cur = conn.cursor()
     insert_script = 'SET search_path = "WorldWineWeb", am4404, public; Select getproducts(20)'
+    cur.execute(insert_script)
+    records = cur.fetchall()
+    data = []
+    data = list(records)
+    rows = []
+    for i in enumerate(data):
+        ##Lägger till värden för en produkt i en lista
+        splitColumns = str(i).split(",")
+        splitColumns = [j.replace('"', '') for j in splitColumns]
+        splitColumns = [j.replace('(', '') for j in splitColumns]
+        splitColumns = [j.replace(')', '') for j in splitColumns]
+        splitColumns = [j.replace("'", '') for j in splitColumns] 
+        ###Lägger till listan i en lista
+        rows.append(splitColumns)
+      
+    conn.commit()
+    return render_template("products.html", rows=rows)
+
+@views.route('/products', methods=['GET', 'POST'])
+def category():
+    
+    print("Products found!")
+    cur = conn.cursor()
+    insert_script = 'SET search_path = "WorldWineWeb", am4404, public; Select getbeers(20)'
     cur.execute(insert_script)
     records = cur.fetchall()
     data = []
